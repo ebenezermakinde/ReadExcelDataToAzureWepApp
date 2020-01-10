8539f432b8e82e8dee8a49c05a60d6f7c7283ac0
Ebenezer Makinde <ebenezermakinde@gmail.com>
2020-01-10 13:39:00 +0100

    Upload published Azure webapp

---
+++ WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Excels()
+        {
+            ViewBag.Message = "the data from excel:";
+            string data = "";
+
+            //your excel path after uploaded, here I hardcoded it for test only
+            string path = @"D:\home\site\wwwroot\Files\ddd.xlsx";
+            var excelData = new ExcelData(path);
+            var people = excelData.GetData("sheet1");
+
+            foreach (var p in people)
+            {
+                for (int i = 0; i <= p.ItemArray.GetUpperBound(0); i++)
+                {
+                    data += p[i].ToString() + ",";
+                }
+
+                data += ";";
+            }
+
+            ViewBag.Message += data;
+
+            return View();
+        }
+    }
+}
---
+++ WebApplication1/ExcelData.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using ExcelDataReader;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data;
+using System.IO;
+using System.Web;
+
+namespace WebApplication1
+{
+    public class ExcelData
+    {
+        string _path;
+        public ExcelData(string path)
+        {
+            _path = path;
+        }
+
+        public IExcelDataReader GetExcelReader()
+        {
+            FileStream stream = File.Open(_path, FileMode.Open, FileAccess.Read);
+            IExcelDataReader reader = null;
+            try
+            {
+                if (_path.EndsWith(".xls"))
+                {
+                    reader = ExcelReaderFactory.CreateBinaryReader(stream);
+                }
+                if (_path.EndsWith(".xlsx"))
+                {
+                    reader = ExcelReaderFactory.CreateOpenXmlReader(stream);
+                }
+
+                return reader;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        //read the sheets name if you need
+        public IEnumerable<string> GetWorksheetNames()
+        {
+            var reader = this.GetExcelReader();
+            var workbook = reader.AsDataSet();
+            var sheets = from DataTable sheet in workbook.Tables select sheet.TableName;
+            return sheets;
+        }
+
+        //read data in a specified sheet
+        public IEnumerable<DataRow> GetData(string sheet)
+        {
+
+            var reader = this.GetExcelReader();
+            var workSheet = reader.AsDataSet(new ExcelDataSetConfiguration()
+            {
+                ConfigureDataTable = (_) => new ExcelDataTableConfiguration()
+                {
+                    //indicates if use the header values
+                    UseHeaderRow = true
+                }
+
+            }).Tables[sheet];
+
+            var rows = from DataRow a in workSheet.Rows select a;
+            return rows;
+        }
+
+    }
+}
